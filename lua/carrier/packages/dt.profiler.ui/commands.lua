92cad0ecf876177c1194b474d7033449eaa59e57
notcake <cakenotfound@googlemail.com>
2017-07-18 01:03:56 +0100

    Added context menu item.

--- lua/carrier/packages/dt.profiler.ui/commands.lua
+++ lua/carrier/packages/dt.profiler.ui/commands.lua
@@ -1,10 +1,19 @@
-function RegisterCommands ()	
+local desktopItem = nil
+
+function RegisterCommands ()
 	concommand.Add ("+dt", function (ply, cmd, args) GetWindow ():SetVisible (true)  end)
 	concommand.Add ("-dt", function (ply, cmd, args) GetWindow ():SetVisible (false) end)
 	
 	concommand.Add ("dt_toggle",
 		function (ply, cmd, args)
 			GetWindow ():SetVisible (not GetWindow ():IsVisible ())
+		end
+	)
+	
+	desktopItem = Phoenix.Desktop:AddItem ("dt", "error")
+	desktopItem.Click:AddListener (
+		function ()
+			GetWindow ():SetVisible (true)
 		end
 	)
 end
@@ -14,4 +23,7 @@ function UnregisterCommands ()
 	concommand.Remove ("-dt")
 	
 	concommand.Remove ("dt_toggle")
+	
+	desktopItem:dtor ()
+	desktopItem = nil
 end
