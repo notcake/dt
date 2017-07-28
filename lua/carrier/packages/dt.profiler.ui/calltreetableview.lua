67c42cc8392d67899de783e1857ffac3f9ebbe2b
notcake <cakenotfound@googlemail.com>
2017-07-28 02:50:33 +0100

    Added CallTreeTableView:SetFrame () stub.

--- lua/carrier/packages/dt.profiler.ui/calltreetableview.lua
+++ lua/carrier/packages/dt.profiler.ui/calltreetableview.lua
@@ -3,9 +3,23 @@ CallTreeTableView = Class (self, Glass.TreeTableView)
 
 function self:ctor (profiler)
 	self.Profiler = profiler
+	self.Frame    = nil
 end
 
 -- CallTreeTableView
 function self:GetProfiler ()
 	return self.Profiler
+end
+
+function self:SetFrame (frame)
+	if self.Frame == frame then return end
+	
+	local previousFrame = self.Frame
+	self.Frame = frame
+	
+	self:ClearItems ()
+	self:AddItem ()
+	for section in self.Frame:GetChildEnumerator () do
+		self:AddItem ()
+	end
 end
