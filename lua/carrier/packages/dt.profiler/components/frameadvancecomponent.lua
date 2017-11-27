e781828aab155f9b730cebbdc805975193ad000d
notcake <cakenotfound@googlemail.com>
2017-11-27 21:20:30 +0000

    Improved default profiler.

--- lua/carrier/packages/dt.profiler/components/frameadvancecomponent.lua
+++ lua/carrier/packages/dt.profiler/components/frameadvancecomponent.lua
@@ -1,12 +1,34 @@
 local self = {}
 Profiler.FrameAdvanceComponent = Class (self, Profiler.ProfilerComponent)
 
+Profiler.FrameAdvanceComponent.FirstHookName = "dt.FrameAdvanceComponent.\xc2\x52\xd1\xfa"
+Profiler.FrameAdvanceComponent.LastHookName  = "dt.FrameAdvanceComponent.\x89\x45\x9b\xad"
+
 function self:ctor (profiler)
 	self.Profiler = profiler
+	
+	self.WorldPanelFirst = nil
+	self.WorldPanelLast  = nil
 end
 
 -- ProfilerComponent
 function self:OnEnabled ()
+	self.WorldPanelFirst = vgui.CreateX ("Panel")
+	self.WorldPanelFirst:SetSize (0, 0)
+	self.WorldPanelFirst:SetZPos (-32768)
+	self.WorldPanelLast = vgui.CreateX ("Panel")
+	self.WorldPanelLast:SetSize (0, 0)
+	self.WorldPanelLast:SetZPos (32767)
+	self.WorldPanelFirst.Think = function ()
+		if self.WorldPanelLast:GetZPos () ~= 32767 then
+			self.WorldPanelLast:SetZPos (32767)
+		end
+		self.Profiler:BeginSection ("vgui.GetWorldPanel() InternalThinkTraverse")
+	end
+	self.WorldPanelLast.Think = function ()
+		self.Profiler:EndSection ()
+	end
+	
 	self:AddPrePostHook ("DrawOpaqueRenderables")
 	self:AddPrePostHook ("DrawTranslucentRenderables")
 	self:AddPrePostHook ("DrawSkyBox")
@@ -14,33 +36,59 @@ function self:OnEnabled ()
 	self:AddPrePostHook ("DrawEffects")
 	self:AddPrePostHook ("DrawHUD")
 	
-	hook.Add ("PreDrawViewModel", "dt.FrameAdvanceComponent",
+	for _, eventName in ipairs ({ "Think", "Tick", "CreateMove", "StartCommand" }) do
+		hook.Add (eventName, Profiler.FrameAdvanceComponent.FirstHookName,
+			function ()
+				self.Profiler:BeginSection (eventName)
+			end
+		)
+		hook.Add (eventName, Profiler.FrameAdvanceComponent.LastHookName,
+			function ()
+				self.Profiler:EndSection ()
+			end
+		)
+	end
+	
+	hook.Add ("SetupMove", Profiler.FrameAdvanceComponent.FirstHookName,
+		function ()
+			self.Profiler:BeginSection ("SetupMove / Move / FinishMove")
+		end
+	)
+	hook.Add ("FinishMove", Profiler.FrameAdvanceComponent.LastHookName,
+		function ()
+			self.Profiler:EndSection ()
+		end
+	)
+	
+	hook.Add ("PreDrawViewModel", Profiler.FrameAdvanceComponent.FirstHookName,
 		function (viewModel, ply, weapon)
 			if not viewModel then return end
 			self.Profiler:BeginSection ("DrawViewModel")
 		end
 	)
-	hook.Add ("PostDrawViewModel", "dt.FrameAdvanceComponent",
+	hook.Add ("PostDrawViewModel", Profiler.FrameAdvanceComponent.LastHookName,
 		function (viewModel, ply, weapon)
 			if not viewModel then return end
 			self.Profiler:EndSection ()
 		end
 	)
 	
-	hook.Add ("PrePlayerDraw", "dt.FrameAdvanceComponent",
+	hook.Add ("PrePlayerDraw", Profiler.FrameAdvanceComponent.FirstHookName,
 		function (ply)
 			self.Profiler:BeginSection ("PlayerDraw (" .. ply:Name () .. ")")
 		end
 	)
-	hook.Add ("PostPlayerDraw", "dt.FrameAdvanceComponent", function (ply) self.Profiler:EndSection () end)
+	hook.Add ("PostPlayerDraw", Profiler.FrameAdvanceComponent.LastHookName, function (ply) self.Profiler:EndSection () end)
 	
-	hook.Add ("PreRender", "dt.FrameAdvanceComponent",
+	hook.Add ("PreRender", Profiler.FrameAdvanceComponent.FirstHookName,
 		function ()
+			-- print (FrameNumber (), "PreRender")
 			self.Profiler:BeginSection ("Render")
 		end
 	)
-	hook.Add ("PostRender", "dt.FrameAdvanceComponent",
+	hook.Add ("PostRender", Profiler.FrameAdvanceComponent.LastHookName,
 		function (_)
+			-- print (FrameNumber (), "PostRender")
 			self.Profiler:EndSection ()
 			
 			local frame = self.Profiler:GetCurrentFrame ()
@@ -55,6 +103,23 @@ function self:OnEnabled ()
 end
 
 function self:OnDisabled ()
+	if self.WorldPanelFirst then
+		self.WorldPanelFirst:Remove ()
+		self.WorldPanelFirst = nil
+	end
+	if self.WorldPanelLast then
+		self.WorldPanelLast:Remove ()
+		self.WorldPanelLast = nil
+	end
+	
+	for _, eventName in ipairs ({ "Think", "Tick", "CreateMove", "StartCommand" }) do
+		hook.Remove (eventName, Profiler.FrameAdvanceComponent.FirstHookName)
+		hook.Remove (eventName, Profiler.FrameAdvanceComponent.LastHookName)
+	end
+	
+	hook.Remove ("SetupMove",  Profiler.FrameAdvanceComponent.FirstHookName)
+	hook.Remove ("FinishMove", Profiler.FrameAdvanceComponent.LastHookName)
+	
 	self:RemovePrePostHook ("Render")
 	self:RemovePrePostHook ("DrawOpaqueRenderables")
 	self:RemovePrePostHook ("DrawTranslucentRenderables")
@@ -67,12 +132,12 @@ end
 
 -- Internal
 function self:AddPrePostHook (name)
-	hook.Add ("Pre" .. name, "dt.FrameAdvanceComponent",
+	hook.Add ("Pre" .. name, Profiler.FrameAdvanceComponent.FirstHookName,
 		function ()
 			self.Profiler:BeginSection (name)
 		end
 	)
-	hook.Add ("Post" .. name, "dt.FrameAdvanceComponent",
+	hook.Add ("Post" .. name, Profiler.FrameAdvanceComponent.LastHookName,
 		function ()
 			self.Profiler:EndSection ()
 		end
@@ -80,6 +145,6 @@ function self:AddPrePostHook (name)
 end
 
 function self:RemovePrePostHook (name)
-	hook.Remove ("Pre"  .. name, "dt.FrameAdvanceComponent")
-	hook.Remove ("Post" .. name, "dt.FrameAdvanceComponent")
+	hook.Remove ("Pre"  .. name, Profiler.FrameAdvanceComponent.FirstHookName)
+	hook.Remove ("Post" .. name, Profiler.FrameAdvanceComponent.LastHookName)
 end
